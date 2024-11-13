2d2e03e82297bb4ad1ffd3ee5d56d6559f4603e9
GonzaloLopezS <gonzalolopezsegovia@gmail.com>
2024-11-13 20:33:23 +0100

    Print de prueba

--- binder_bateaus.py
+++ binder_bateaus.py
@@ -2,5 +2,6 @@ import numpy as np
 import pandas as pd
 from marinetrafficapi import MarineTrafficApi
 
+print("Cargando conexion a Marine Traffice API")
 api = MarineTrafficApi(api_key="565b636b073b55b4de19a39b55c2a3cde88c4f3f")
 vessel_positions = api.vessel_historical_track()
