828126c8af4342cca949d22296711c36022fd24e
Gonzalo Lopez <gonzalolopezsegovia@gmail.com>
2024-12-10 23:21:50 +0100

    Creación "scrapping_functions.py" + log.log

--- binder_bateaus.py
+++ binder_bateaus.py
@@ -1,6 +1,41 @@
-import numpy as np
-import pandas as pd
-from marinetrafficapi import MarineTrafficApi
+# Opcion C: Acceso a datos en kaggle o local:
 
-api = MarineTrafficApi(api_key="565b636b073b55b4de19a39b55c2a3cde88c4f3f")
-vessel_positions = api.vessel_historical_track()
+# 1) AIS open source data of China's coastal waters: https://www.kaggle.com/datasets/yccwcc/aisdata
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("yccwcc/aisdata")
+
+print("Path to dataset files:", path)
+
+# 2) AIS data Jan-April 2023: https://www.kaggle.com/datasets/singhkunal/ais-all-data-jan01-apr04-2023
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("singhkunal/ais-all-data-jan01-apr04-2023")
+
+print("Path to dataset files:", path)
+
+# 3) AIS DAtaset Port of Tallinn: https://www.kaggle.com/datasets/gaborvisky/ais-dataset
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("gaborvisky/ais-dataset")
+
+print("Path to dataset files:", path)
+
+# 4) Global Transshipment Behaviour
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("sayansh001/global-transshipment-behaviour")
+
+print("Path to dataset files:", path)
+
+# 5) AIS Data For Ships: https://www.kaggle.com/datasets/marsalanakhtar/ais-data-for-ships
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("marsalanakhtar/ais-data-for-ships")
+
+print("Path to dataset files:", path)
