8ac14aafd9ab2ba81a1767b37c7dda05e2eddce0
Gonzalo Lopez <gonzalolopezsegovia@gmail.com>
2025-01-15 11:42:21 +0100

    datasets added

--- ais_noaa_process.py
+++ ais_noaa_process.py
@@ -3,6 +3,8 @@ import zipfile
 import os
 import requests
 from bs4 import BeautifulSoup
+import re
+import calendar
 
 # 1. scraping a página de zips
 def web_scraping(url_in, href_in):
@@ -63,7 +65,7 @@ def data_transformation(df_in, vessel_type_list, csv_filename_in):
     df_cargo.sort_values(by = ['Length'], ascending= False)
 
     # Guardar dataframe procesado:
-    dataframe_procesado = "df_procesado_" + csv_filename_in.split("/")[1]
+    dataframe_procesado = directory + "/" + "df_procesado_" + csv_filename_in.split("/")[1]
     df_cargo.to_csv(dataframe_procesado, index = False)
 
 # 6. eliminar .zip y fichero original
@@ -80,29 +82,33 @@ def file_elimination(csv_original_in, zip_original_in):
 # ----------------------------------
 url = "https://coast.noaa.gov/htdata/CMSP/AISDataHandler/2024/index.html"
 directory = 'ais_noaa_gov'
-zip_filename = 'AIS_2024_09_29.zip' # variable con los nombres de los ficheros
-path_to_zip_file = directory + '/' + zip_filename
+
+# crear expresion regular para obtener todos los .zip:
+base_name = 'AIS_2024_09' # variable con los nombres de los ficheros
+zip_filenames = [f"{base_name}_{str(i).zfill(2)}.zip" for i in range(1, 31)]
 cargo_vessels = [70.0, 71.0, 72.0, 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0]
-href_in = zip_filename
 
-# Captura de datos:
-file_link = web_scraping(url, href_in)
-csv_filename = download_zip(file_link, path_to_zip_file)
-uncompress_zip(csv_filename, path_to_zip_file, directory)
+# 7. repetir 1-6
+for i in zip_filenames:
+    path_to_zip_file = directory + '/' + i
+    href_in = i
+    print(path_to_zip_file)
+    print(i)
 
-# Extraccion y Transformación:
-dataframe_csv = data_extraction(csv_filename)
-data_transformation(dataframe_csv, cargo_vessels, csv_filename)
+    # Captura de datos:
+    file_link = web_scraping(url, href_in)
+    csv_filename = download_zip(file_link, path_to_zip_file)
+    uncompress_zip(csv_filename, path_to_zip_file, directory)
+
+    # Extraccion y Transformación:
+    dataframe_csv = data_extraction(csv_filename)
+    data_transformation(dataframe_csv, cargo_vessels, csv_filename)
+
+    # Eliminacion del fichero .zip una vez extraidos los datos:
+    file_elimination(csv_filename, path_to_zip_file)
 
 # Carga del nuevo dataframe:
-
 # dataframe_loaded = pd.read_csv(directory + "/" + dataframe_procesado)
-
-# Eliminacion del fichero .zip una vez extraidos los datos:
-file_elimination(csv_filename, path_to_zip_file)
-
-# ----------------------------------
-# 7. repetir 1-6
 
 # Revisar variables que manejan directory y filenames.
 # Igual es posible extender su uso a spark. Problema: limite de librerias.
