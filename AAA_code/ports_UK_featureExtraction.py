4104653fb39b910d95ed90b01817e2bc8d20d530
Gonzalo Lopez <gonzalolopezsegovia@gmail.com>
2025-01-19 17:20:51 +0100

    actualizacion estudio uk

--- AAA_code/ports_UK_featureExtraction.py
+++ AAA_code/ports_UK_featureExtraction.py
@@ -3,5 +3,6 @@ import pandas as pd
 # Crear libreria para descomprimir:
 
 # 1. Descomprimir .zip
+# quarterly-port-freight-statistics-july-to-september-2024-tables.zip
 
 # 2. Extraer informacion de los .csv
